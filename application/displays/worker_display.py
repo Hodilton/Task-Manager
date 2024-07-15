c02348a9527eac68515eba06f0098c1986f18ba9
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-07-16 05:20:33 +0700

    Оптимизировал WorkerDisplay

--- application/displays/worker_display.py
+++ application/displays/worker_display.py
@@ -5,7 +5,8 @@ from .base_display import BaseDisplay
 class WorkerDisplay(BaseDisplay):
     def __init__(self, parent, db_manager):
         self.db_table = db_manager.tables['worker']['instance']
-        self.db_manager = db_manager
+        self.inputs = {}
+
         super().__init__(parent, db_manager)
 
     def initialize_display(self):
@@ -13,41 +14,39 @@ class WorkerDisplay(BaseDisplay):
         self.create_entries()
 
     def create_entries(self):
-        # Last Name
-        self.lastname_label = tk.Label(self.bottom_frame, text="Фамилия")
-        self.lastname_label.grid(row=1, column=0, padx=5, pady=5, sticky='w')
-        self.lastname_entry = tk.Entry(self.bottom_frame, width=40)
-        self.lastname_entry.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky='w')
+        self.inputs['lastname'] = self.create_label_entry("Фамилия", 1)
+        self.inputs['firstname'] = self.create_label_entry("Имя", 3)
+        self.inputs['middlename'] = self.create_label_entry("Отчество", 5)
+        self.inputs['workerpost'] = self.create_combobox("Должность", 7)
 
-        # First Name
-        self.firstname_label = tk.Label(self.bottom_frame, text="Имя")
-        self.firstname_label.grid(row=3, column=0, padx=5, pady=5, sticky='w')
-        self.firstname_entry = tk.Entry(self.bottom_frame, width=40)
-        self.firstname_entry.grid(row=4, column=0, columnspan=2, padx=5, pady=5, sticky='w')
+    def create_label_entry(self, label_text, row):
+        label = tk.Label(self.bottom_frame, text=label_text)
+        label.grid(row=row, column=0, padx=5, pady=5, sticky='w')
 
-        # Middle Name
-        self.middlename_label = tk.Label(self.bottom_frame, text="Отчество")
-        self.middlename_label.grid(row=5, column=0, padx=5, pady=5, sticky='w')
-        self.middlename_entry = tk.Entry(self.bottom_frame, width=40)
-        self.middlename_entry.grid(row=6, column=0, columnspan=2, padx=5, pady=5, sticky='w')
+        entry = tk.Entry(self.bottom_frame, width=40)
+        entry.grid(row=row+1, column=0, columnspan=2, padx=5, pady=5, sticky='w')
 
-        # Worker Post (Dropdown)
-        self.workerpost_label = tk.Label(self.bottom_frame, text="Должность")
-        self.workerpost_label.grid(row=7, column=0, padx=5, pady=5, sticky='w')
-        self.workerpost_combobox = ttk.Combobox(self.bottom_frame, state="readonly", width=37)
-        self.workerpost_combobox.grid(row=8, column=0, columnspan=2, padx=5, pady=5, sticky='w')
+        return entry
 
-        self.update_combobox()
+    def create_combobox(self, label_text, row):
+        label = tk.Label(self.bottom_frame, text=label_text)
+        label.grid(row=row, column=0, padx=5, pady=5, sticky='w')
 
-    def update_combobox(self):
+        combobox = ttk.Combobox(self.bottom_frame, state="readonly", width=37)
+        combobox.grid(row=row+1, column=0, columnspan=2, padx=5, pady=5, sticky='w')
+
+        self.update_combobox(combobox)
+        return combobox
+
+    def update_combobox(self, combobox):
         worker_posts = self.db_manager.tables['worker_post']['instance'].fetch_all_data()
-        self.workerpost_combobox['values'] = [post[1] for post in worker_posts]
+        combobox['values'] = [post[1] for post in worker_posts]
 
     def add_item(self):
-        lastname = self.lastname_entry.get().strip()
-        firstname = self.firstname_entry.get().strip()
-        middlename = self.middlename_entry.get().strip()
-        worker_post_name = self.workerpost_combobox.get().strip()
+        lastname = self.inputs['lastname'].get().strip()
+        firstname = self.inputs['firstname'].get().strip()
+        middlename = self.inputs['middlename'].get().strip()
+        worker_post_name = self.inputs['workerpost'].get().strip()
 
         if lastname and firstname and middlename and worker_post_name:
             worker_posts = self.db_manager.tables['worker_post']['instance'].fetch_all_data()
@@ -55,15 +54,19 @@ class WorkerDisplay(BaseDisplay):
 
             if worker_post_id:
                 self.db_table.insert_data((lastname, firstname, middlename, worker_post_id))
-                self.lastname_entry.delete(0, tk.END)
-                self.firstname_entry.delete(0, tk.END)
-                self.middlename_entry.delete(0, tk.END)
-                self.workerpost_combobox.set('')
+                self.clear_entries()
                 self.update_listbox()
             else:
                 messagebox.showerror("Ошибка", "Выбранная должность не найдена")
         else:
             messagebox.showerror("Ошибка", "Заполните все поля")
+
+    def clear_entries(self):
+        for input_widget in self.inputs.values():
+            if isinstance(input_widget, tk.Entry):
+                input_widget.delete(0, tk.END)
+            elif isinstance(input_widget, ttk.Combobox):
+                input_widget.set('')
 
     def delete_item(self):
         selected_index = self.listbox.curselection()
@@ -76,12 +79,9 @@ class WorkerDisplay(BaseDisplay):
 
     def update_listbox(self):
         super().update_listbox()
-
         data = self.db_table.fetch_all_data()
 
-        if not data:
-            return
-
-        for row in data:
-            display_string = f"{row[0]}: {row[1]} {row[2]} {row[3]} - {row[4]}"
-            self.listbox.insert(tk.END, display_string)
+        if data:
+            for row in data:
+                display_string = f"{row[0]}: {row[1]} {row[2]} {row[3]} - {row[4]}"
+                self.listbox.insert(tk.END, display_string)
