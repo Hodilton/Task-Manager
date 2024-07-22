1b42b240fc7cf83969f14f83332a0b4adec07311
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-07-22 20:15:49 +0700

    Поправил структуру+отладка

--- application/app_gui.py
+++ application/app_gui.py
@@ -1,17 +1,15 @@
 import importlib
 import tkinter as tk
 
+from utilities.messages import ErrorMessages
+
 class ApplicationGUI:
     def __init__(self, root, displays_config, db_manager):
         self.root = root
         self.db_manager = db_manager
+        self.displays = {}
 
-        self.initialize_gui(displays_config)
-
-    def initialize_gui(self, displays_config):
         self.create_main_frame()
-        self.create_selector_frame()
-        self.create_display_frame()
         self.initialize_displays(displays_config)
         self.create_selector_buttons(displays_config)
 
@@ -22,6 +20,9 @@ class ApplicationGUI:
         self.main_frame = tk.Frame(self.root)
         self.main_frame.pack(fill=tk.BOTH, expand=True)
 
+        self.create_selector_frame()
+        self.create_display_frame()
+
     def create_selector_frame(self):
         self.selector_frame = tk.Frame(self.main_frame)
         self.selector_frame.pack(side=tk.LEFT, fill=tk.Y)
@@ -31,8 +32,6 @@ class ApplicationGUI:
         self.display_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
     def initialize_displays(self, displays_config):
-        self.display_instances = {}
-
         for key, display_config in displays_config.items():
             module_name = display_config['module']
             class_name = display_config['class_name']
@@ -40,10 +39,11 @@ class ApplicationGUI:
             try:
                 module = importlib.import_module(module_name)
                 display_class = getattr(module, class_name)
-                self.display_instances[key] = display_class(self.display_frame, self.db_manager)
 
-            except (ModuleNotFoundError, AttributeError) as e:
-                print(f"Error loading display {key}: {e}")
+                self.displays[key] = display_class(self.display_frame, self.db_manager)
+
+            except Exception as e:
+                print(ErrorMessages.Database.DISPLAY_LOADING.format(display_name=class_name, error=e))
 
     def create_selector_buttons(self, displays_config):
         for key, display_config in displays_config.items():
@@ -56,10 +56,11 @@ class ApplicationGUI:
 
     def switch_display(self, display_key):
         self.hide_all_displays()
-        self.current_display = self.display_instances[display_key]
+
+        self.current_display = self.displays[display_key]
         self.current_display.show()
         self.current_display.update_data()
 
     def hide_all_displays(self):
-        for display in self.display_instances.values():
+        for display in self.displays.values():
             display.hide()
