2bc287ae93c355f9e88befdc01e5a8935eed9896
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-07-22 20:15:25 +0700

    Поправил структуру

--- application/app_main.py
+++ application/app_main.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 
 from application.app_gui import ApplicationGUI
-
+from utilities.error_handler import ErrorHandler
 
 class Application:
     def __init__(self, config, db_manager):
-        self.root = tk.Tk()
-        self.root.title("Manager")
-        self.db_manager = db_manager
+        try:
+            self.root = tk.Tk()
+            self.root.title("Manager")
+            self.db_manager = db_manager
 
-        self.initialize_application(config)
+            self.initialize_application(config)
+            self.root.mainloop()
+
+        except Exception as e:
+            ErrorHandler.show_error(str(e))
 
     def initialize_application(self, config):
         self.gui = ApplicationGUI(self.root, config, self.db_manager)
-        self.root.mainloop()
